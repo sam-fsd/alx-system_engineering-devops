8383f91567d1a1c21cf9fb3d0597362d34a0b191
sam-fsd <kabirusam037@gmail.com>
2023-10-23 14:16:13 -0700

    pep* compliance

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,7 +8,9 @@ if __name__ == '__main__':
     r = requests.get(
         f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
     )
-    r_emp = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    r_emp = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+    )
     emp_name = r_emp.json().get('name')
 
     emp_todos = r.json()
@@ -18,7 +20,10 @@ if __name__ == '__main__':
         if todo.get('completed'):
             done_tasks += 1
 
-    print(f'Employee {emp_name} is done with tasks({done_tasks}/{len(emp_todos)}):')
+    print(
+        f'Employee {emp_name} is done with tasks'
+        f'({done_tasks}/{len(emp_todos)}):'
+    )
 
     for task in emp_todos:
         if task.get('completed'):
