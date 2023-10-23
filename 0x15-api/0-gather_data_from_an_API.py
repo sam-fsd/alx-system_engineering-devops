dcffab1e4017b124c098f6c5ef5fec1821256f9c
sam-fsd <kabirusam037@gmail.com>
2023-10-23 14:07:14 -0700

    Import safety

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Communicate with a RESTful API"""
+"""Communicate with a RESTful API."""
 
 import requests
 from sys import argv
 
+if __name__ == '__main__':
+    r = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
+    )
+    r_emp = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    emp_name = r_emp.json().get('name')
 
-r = requests.get(
-    f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
-)
-r_emp = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
-emp_name = r_emp.json().get('name')
+    emp_todos = r.json()
+    done_tasks = 0
 
-emp_todos = r.json()
-done_tasks = 0
+    for todo in emp_todos:
+        if todo.get('completed'):
+            done_tasks += 1
 
-for todo in emp_todos:
-    if todo.get('completed'):
-        done_tasks += 1
+    print(f'Employee {emp_name} is done with tasks({done_tasks}/{len(emp_todos)}):')
 
-print(f'Employee {emp_name} is done with tasks({done_tasks}/{len(emp_todos)}):')
-
-for task in emp_todos:
-    if task.get('completed'):
-        print(f"     {task.get('title')}")
+    for task in emp_todos:
+        if task.get('completed'):
+            print(f"     {task.get('title')}")
